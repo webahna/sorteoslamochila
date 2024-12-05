29892c9ce1dcaa7261f5ac66576952a0f95c9b4b
webahna <alexrmzqzda@gmail.com>
2024-12-05 15:57:53 -0700

    commit051224-4pm

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -5,14 +5,15 @@ import { Router, Routes } from '@angular/router';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+  styleUrls: ['./index.component.scss'],
 })
 export class IndexComponent {
-  constructor(private router: Router) { }
-  ngOnInit( ) {
-   }
-  
-   goToComprar(){
+  constructor(private router: Router) {}
+  ngOnInit() {
+    console.log('Hala Madrid');
+  }
+
+  goToComprar() {
     this.router.navigate(['comprar']);
   }
 }
